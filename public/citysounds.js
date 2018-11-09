bb7f2d0d4bce63f20a20df964517b83e352e5de9
Josh Simani <jsimani@gmail.com>
2018-11-09 15:00:12 -0500

    Added non-functioning login button to map

--- public/citysounds.js
+++ public/citysounds.js
@@ -38,7 +38,14 @@ function initMap()
 	};   
 				
 	map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-    infoWindow = new google.maps.InfoWindow()
+    infoWindow = new google.maps.InfoWindow();
+
+    var loginControlDiv = document.createElement('div');
+    var centerControl = new LoginControl(loginControlDiv, map);
+
+    loginControlDiv.index = 1;
+    map.controls[google.maps.ControlPosition.TOP_RIGHT].push(loginControlDiv);
+
 
     getCurrentLocation();
     createMarkers();
@@ -103,5 +110,35 @@ function initializeWindow(city) {
     });
 }
 
+function LoginControl(controlDiv, map) {
+    // Set CSS for the control border.
+    var controlUI = document.createElement('div');
+    controlUI.style.backgroundColor = '#fff';
+    controlUI.style.border = '2px solid #fff';
+    controlUI.style.borderRadius = '3px';
+    controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+    controlUI.style.cursor = 'pointer';
+    controlUI.style.marginBottom = '22px';
+    controlUI.style.textAlign = 'center';
+    controlUI.title = 'Click to login to Spotify';
+    controlDiv.appendChild(controlUI);
+
+    // Set CSS for the control interior.
+    var controlText = document.createElement('div');
+    controlText.style.color = 'rgb(25,25,25)';
+    controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+    controlText.style.fontSize = '16px';
+    controlText.style.lineHeight = '38px';
+    controlText.style.paddingLeft = '5px';
+    controlText.style.paddingRight = '5px';
+    controlText.innerHTML = 'Login to Spotify';
+    controlUI.appendChild(controlText);
+
+    // Setup the click event listeners: simply set the map to Chicago.
+    controlUI.addEventListener('click', function() {
+        console.log("Clicked login button");
+    });
+
+}
 
 
