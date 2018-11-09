ac63658bd656cd1ce87195165863f570923d07ee
Josh Simani <jsimani@gmail.com>
2018-11-09 15:26:18 -0500

    Fixed styling for login button

--- public/citysounds.js
+++ public/citysounds.js
@@ -111,26 +111,13 @@ function initializeWindow(city) {
 }
 
 function LoginControl(controlDiv, map) {
-    // Set CSS for the control border.
     var controlUI = document.createElement('div');
-    controlUI.style.backgroundColor = '#fff';
-    controlUI.style.border = '2px solid #fff';
-    controlUI.style.borderRadius = '3px';
-    controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
-    controlUI.style.cursor = 'pointer';
-    controlUI.style.marginBottom = '22px';
-    controlUI.style.textAlign = 'center';
+    controlUI.setAttribute("id", "loginControlUI");
     controlUI.title = 'Click to login to Spotify';
     controlDiv.appendChild(controlUI);
 
-    // Set CSS for the control interior.
     var controlText = document.createElement('div');
-    controlText.style.color = 'rgb(25,25,25)';
-    controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
-    controlText.style.fontSize = '16px';
-    controlText.style.lineHeight = '38px';
-    controlText.style.paddingLeft = '5px';
-    controlText.style.paddingRight = '5px';
+    controlText.setAttribute("id", "loginControlText");
     controlText.innerHTML = 'Login to Spotify';
     controlUI.appendChild(controlText);
 
