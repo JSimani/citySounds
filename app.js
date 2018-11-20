02f38286d4ff4a1b8bd31f1041e9cee1ba7fa2f2
Josh Simani <jsimani@gmail.com>
2018-11-19 22:25:48 -0500

    Changed callback function

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var cookieParser = require('cookie-parser');
 
 var client_id = '8e661acc4ece4137913cb751c3a8c1df'; // Your client id
 var client_secret = '95f5184b63484c709a71ecc87851bd07'; // Your secret
-var redirect_uri = 'http://citysounds20.herokuapp.com/callback'; // Your redirect uri
+var redirect_uri = 'http://citysounds20.herokuapp.com//callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
