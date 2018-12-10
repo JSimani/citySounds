3660d732dd14be04495f9bf2a67484ac9d5143c0
Josh Simani <jsimani@gmail.com>
2018-12-09 23:55:19 -0500

    Restored links to albums and artists

--- public/assets/javascript/init.js
+++ public/assets/javascript/init.js
@@ -6,7 +6,7 @@ var device = null;
 
 function initialize()
 {
-	map = initMap();
+    map = initMap();
     
     addLoginButton(map);
     infoWindow = addOverlay();
@@ -57,7 +57,7 @@ function initializeInfoWindow(marker) {
 
             for (var i = 0; i < marker.songs.length; i++) {
                 var curTrack = marker.songs[i];
-                info += "<p><a id='link' href='" + curTrack.external_urls.spotify + "' target='_blank'>" + curTrack.name + " by " + curTrack.artists[0].name + "</a></p>";
+                info += "<p><a class='link' href='" + curTrack.external_urls.spotify + "' target='_blank'>" + curTrack.name + " by " + curTrack.artists[0].name + "</a></p>";
             }
         }
 
@@ -70,11 +70,12 @@ function initializeInfoWindow(marker) {
 
             for (var i = 0; i < marker.albums.length; i++) {
                 var curAlbum = marker.albums[i];
+                    
+                info += "<p><a class='link' href=' " + curAlbum.external_urls.spotify + "' target='_blank'>" + curAlbum.name + " by " + curAlbum.artists[0].name + "&emsp;</a>";
                 if (localStorage.account_type == "premium") {
-                    info += "<p><a class='link' onclick='playMedia(\"" + curAlbum.uri + "\");'>" + curAlbum.name + " by " + curAlbum.artists[0].name + "&emsp;</a><a id='control'><img src='assets/images/play.png' onclick='playMedia(\"" + curAlbum.uri + "\");' alt='play'></a></p>";
-                } else {
-                    info += "<p><a href='" + curAlbum.external_urls.spotify + "' target='_blank'>" + curAlbum.name + " by " + curAlbum.artists[0].name + "</a></p>";
+                    info += "<a class='control'><img src='assets/images/play.png' onclick='playMedia(\"" + curAlbum.uri + "\");' alt='play'></a>";
                 }
+                info += "</p>";
             }
         }
 
@@ -87,11 +88,12 @@ function initializeInfoWindow(marker) {
 
             for (var i = 0; i < marker.artists.length; i++) {
                 var curArtist = marker.artists[i];
+                
+                info += "<p><a class='link' href=' " + curArtist.external_urls.spotify + "' target='_blank'>" + curArtist.name + "&emsp;</a>";
                 if (localStorage.account_type == "premium") {
-                    info += "<p><a class='link' onclick='playMedia(\"" + curArtist.uri + "\");'>" + curArtist.name + "&emsp;</a><a id='control'><img src='assets/images/play.png' onclick='playMedia(\"" + curArtist.uri + "\");' alt='play'></a></p>";
-                } else {
-                    info += "<p><a href='" + curArtist.external_urls.spotify + "' target='_blank'>" + curArtist.name + "</a></p>";
+                    info += "<a class='control'><img src='assets/images/play.png' onclick='playMedia(\"" + curArtist.uri + "\");' alt='play'></a>";
                 }
+                info += "</p>";
             }
         }
     }
