b84f4b3219b03eb688428fc4e7e5e7fe07814ab8
Josh Simani <jsimani@gmail.com>
2018-12-19 01:18:59 -0500

    Modified environment variables for configuration

--- app.js
+++ app.js
@@ -7,17 +7,22 @@
  * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
  */
 
-var keys = require('./config');
 var express = require('express'); // Express web server framework
 var request = require('request'); // "Request" library
 var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = keys.client_id;; // Your client id
-var client_secret = keys.client_secret; // Your secret
+var client_id = process.env.CLIENT_ID || 'null'; // Your client id
+var client_secret = process.env.CLIENT_SECRET || 'null'; // Your secret
 // var redirect_uri = 'http://localhost:8000/callback';
 var redirect_uri = 'http://citysounds20.herokuapp.com/callback';
+
+if (client_id == 'null') {
+    var keys = require('./config');
+    client_id = keys.client_id;
+    client_secret = keys.client_secret;
+}
 
 var generateRandomString = function(length) {
     var text = '';
