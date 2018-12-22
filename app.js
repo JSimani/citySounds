3f38b11c3cbbb9275749d5fd455954ca935990d9
Josh Simani <jsimani@gmail.com>
2018-12-22 15:25:13 -0500

    Restructured public directory

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var cookieParser = require('cookie-parser');
 
 var client_id = process.env.CLIENT_ID; // Your client id
 var client_secret = process.env.CLIENT_SECRET; // Your secret
-var redirect_uri = 'http://localhost:8000/callback';
-// var redirect_uri = 'http://citysounds20.herokuapp.com/callback';
+// var redirect_uri = 'http://localhost:8000/callback';
+var redirect_uri = 'http://citysounds20.herokuapp.com/callback';
 
 if (!client_id) {
     var keys = require('./config');
